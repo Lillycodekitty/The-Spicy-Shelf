d276ba0d69f61771c1bffc825f83f3551f711484
Lillycodekitty <alishafouche07@gmail.com>
2025-07-05 15:25:11 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,15 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0D0JTSLLRv0pNUHrZwv7Wqu6ATCbWfDY",
+  authDomain: "the-spicy-shelf.firebaseapp.com",
+  projectId: "the-spicy-shelf",
+  storageBucket: "the-spicy-shelf.firebasestorage.app",
+  messagingSenderId: "1068936230419",
+  appId: "1:1068936230419:web:0d30101d04ef91a63a31e6"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC0D0JTSLLRv0pNUHrZwv7Wqu6ATCbWfDY",
-    authDomain: "the-spicy-shelf.firebaseapp.com",
-    projectId: "the-spicy-shelf",
-    storageBucket: "the-spicy-shelf.firebasestorage.app",
-    messagingSenderId: "1068936230419",
-    appId: "1:1068936230419:web:0d30101d04ef91a63a31e6"
-  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Auth reference
+const auth = firebase.auth();
